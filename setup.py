d2a702be9438f4a79cee23edb5b38d51e109f922
Marco Heisig <marco.heisig@fau.de>
2018-05-17 13:10:38 +0200

    Fix package metadata and README encoding.

--- setup.py
+++ setup.py
@@ -8,10 +8,15 @@ from setuptools import setup, find_packages
 from codecs import open
 from os import path
 
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
 setup(
     name='cl4py',
-    version='1.0.1',
+    version='1.0.5',
     description='Common Lisp for Python',
+    long_description=readme(),
     license='MIT',
     url='https://github.com/marcoheisig/cl4py',
     author='Marco Heisig',
@@ -19,7 +24,6 @@ setup(
     packages=find_packages(exclude=['contrib', 'docs', 'test']),
     install_requires=[],
     extras_require={},
-    long_description=long_description,
     keywords='foreign functions FFI',
     classifiers=[
         'Development Status :: 3 - Alpha',
@@ -31,8 +35,4 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Lisp' ,
     ],
-
-    package_data={
-        'cl4py': ['cl4py/py.lisp'],
-    },
 )
