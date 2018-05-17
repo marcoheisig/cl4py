2166cb0c51bb1b584e7f389c8ab39cceca2b1ae0
Marco Heisig <marco.heisig@fau.de>
2018-05-17 11:04:36 +0200

    Convert README from Org-mode to Markdown.

--- setup.py
+++ setup.py
@@ -8,14 +8,9 @@ from setuptools import setup, find_packages
 from codecs import open
 from os import path
 
-here = path.abspath(path.dirname(__file__))
-
-with open(path.join(here, 'README.org'), encoding='utf-8') as f:
-    long_description = f.read()
-
 setup(
     name='cl4py',
-    version='1.0.0',
+    version='1.0.1',
     description='Common Lisp for Python',
     license='MIT',
     url='https://github.com/marcoheisig/cl4py',
@@ -24,12 +19,7 @@ setup(
     packages=find_packages(exclude=['contrib', 'docs', 'test']),
     install_requires=[],
     extras_require={},
-    python_requires='>=3.5',
     long_description=long_description,
-
-    # content_type is actually org-mode, but setup.py is quite
-    # restrictive in this respect...
-    long_description_content_type='text/plain',
     keywords='foreign functions FFI',
     classifiers=[
         'Development Status :: 3 - Alpha',
@@ -44,10 +34,5 @@ setup(
 
     package_data={
         'cl4py': ['cl4py/py.lisp'],
-    },
-
-    project_urls={
-        'Bug Reports': 'https://github.com/marcoheisig/cl4py/issues',
-        'Source': 'https://github.com/marcoheisig/cl4py',
     },
 )
