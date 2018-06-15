28341fb710220ab472700e9557e1f71daa7d3c81
Marco Heisig <marco.heisig@fau.de>
2018-06-15 13:51:29 +0200

    Improve handling of circularity in string tokens.

--- cl4py/circularity.py
+++ cl4py/circularity.py
@@ -13,6 +13,44 @@ class SharpsignEquals:
         self.obj = obj
 
 
+def circularize(obj):
+    """Modify and return OBJ, such that each instance of SharpsignEquals or
+    SharpsignSharpsign is replaced by the corresponding object.
+    """
+    table = {}
+    def copy(obj):
+        if isinstance(obj, SharpsignEquals):
+            result = copy(obj.obj)
+            table[obj.label] = result
+            return result
+        elif isinstance(obj, Cons):
+            return Cons(copy(obj.car),
+                        copy(obj.cdr))
+        elif isinstance(obj, list):
+            return list(copy(elt) for elt in obj)
+        else:
+            return obj
+    def finalize(obj):
+        if isinstance(obj, Cons):
+            if isinstance(obj.car, SharpsignSharpsign):
+                obj.car = table[obj.car.label]
+            else:
+                finalize(obj.car)
+            if isinstance(obj.cdr, SharpsignSharpsign):
+                obj.cdr = table[obj.cdr.label]
+            else:
+                finalize(obj.cdr)
+        elif isinstance(obj, list):
+            for i in range(len(obj)):
+                if isinstance(obj[i], SharpsignSharpsign):
+                    obj[i] = table[obj[i].label]
+                else:
+                    finalize(obj[i])
+    result = copy(obj)
+    finalize(result)
+    return result
+
+
 def decircularize(obj, readtable):
     """Return a structure that is similar to OBJ, but where each circularity
 has been replaced by appropriate SharpsignEquals and SharpsignSharpsign
@@ -28,8 +66,8 @@ instances.
             raise RuntimeError('The string "' + s + '" contains more than one token.')
         except EOFError:
             pass
-        str_tokens[s] = token
         if not isinstance(token, str):
+            str_tokens[s] = token
             scan(token)
     # Phase 1: Scan the data and number all circular objects.
     table = {}
@@ -37,6 +75,7 @@ instances.
     def scan(obj):
         if isinstance(obj, str):
             scan_str(obj)
+            return
         nonlocal n
         atom = not (isinstance(obj, Cons) or
                     isinstance(obj, list) or
@@ -90,47 +129,13 @@ instances.
                 for key, val in obj.items():
                     result[copy(key)] = copy(val)
             elif isinstance(obj, str):
-                return str_tokens[obj]
+                if obj in str_tokens:
+                    return copy(str_tokens[obj])
+                else:
+                    return obj
             if n > 0:
                 return SharpsignEquals(n, result)
             else:
                 return result
     return copy(obj)
 
-
-def circularize(obj):
-    """Modify and return OBJ, such that each instance of SharpsignEquals or
-    SharpsignSharpsign is replaced by the corresponding object.
-    """
-    table = {}
-    def copy(obj):
-        if isinstance(obj, SharpsignEquals):
-            result = copy(obj.obj)
-            table[obj.label] = result
-            return result
-        elif isinstance(obj, Cons):
-            return Cons(copy(obj.car),
-                        copy(obj.cdr))
-        elif isinstance(obj, list):
-            return list(copy(elt) for elt in obj)
-        else:
-            return obj
-    def finalize(obj):
-        if isinstance(obj, Cons):
-            if isinstance(obj.car, SharpsignSharpsign):
-                obj.car = table[obj.car.label]
-            else:
-                finalize(obj.car)
-            if isinstance(obj.cdr, SharpsignSharpsign):
-                obj.cdr = table[obj.cdr.label]
-            else:
-                finalize(obj.cdr)
-        elif isinstance(obj, list):
-            for i in range(len(obj)):
-                if isinstance(obj[i], SharpsignSharpsign):
-                    obj[i] = table[obj[i].label]
-                else:
-                    finalize(obj[i])
-    result = copy(obj)
-    finalize(result)
-    return result
