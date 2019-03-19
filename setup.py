cf3fedc5e065c510fdbedd4844b2a72e2826be5d
Marco Heisig <marco.heisig@fau.de>
2019-03-19 13:56:22 +0100

    Depend on Numpy.

--- setup.py
+++ setup.py
@@ -14,14 +14,15 @@ def readme():
 
 setup(
     name='cl4py',
-    version='1.3.2',
+    version='1.4.1',
     description='Common Lisp for Python',
     long_description=readme(),
+    long_description_content_type='text/x-rst',
     license='MIT',
     url='https://github.com/marcoheisig/cl4py',
     author='Marco Heisig',
     author_email='marco.heisig@fau.de',
-    install_requires=[],
+    install_requires=['numpy'],
     extras_require={},
     keywords='foreign functions FFI',
     classifiers=[
@@ -32,6 +33,7 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Programming Language :: Lisp' ,
     ],
     packages=find_packages(exclude=['contrib', 'docs', 'test']),
