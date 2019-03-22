a236f5c273197968095a14fb372a0c030c649be3
Marco Heisig <marco.heisig@fau.de>
2019-03-22 15:38:18 +0100

    Bump version number.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def readme():
 
 setup(
     name='cl4py',
-    version='1.5.4',
+    version='1.5.5',
     description='Common Lisp for Python',
     long_description=readme(),
     long_description_content_type='text/x-rst',
