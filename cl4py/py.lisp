a0c4a30a847dfca1612778e0e844375ebd71c359
Marco Heisig <marco.heisig@fau.de>
2019-03-25 14:32:37 +0100

    Fix writing of rank one specialized arrays.

--- cl4py/py.lisp
+++ cl4py/py.lisp
@@ -617,7 +617,7 @@
   (with-output-to-string (stream nil :element-type 'base-char)
     (format stream "{'descr': '~A', ~
                      'fortran_order': ~:[False~;True~], ~
-                     'shape': (~{~D~^, ~}), }"
+                     'shape': (~{~D,~^ ~}), }"
             (dtype-code (dtype-from-type (array-element-type array)))
             nil
             (array-dimensions array))))
